03e9bb678872a158cb567bc7dbe7ac80711a203e
Luc Paoli <39316550+lucpaoli@users.noreply.github.com>
2023-11-20 03:44:38 +0000

    I think it WORKS

---
+++ jobs/17_all_quiet_on_the_western_front/2_att_good_moreecpu_1e-3/run_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=32:mem=200gb
+#PBS -lwalltime=20:00:00
+#PBS -N no_pre
+
+# PBS_O_WORKDIR and TMPDIR are both loaded as env variables
+# better to copy scripts to and from TMPDIR
+cd ${PBS_O_WORKDIR}
+
+# Load production environment first
+module load tools/prod
+
+# run the program
+# python myprog.py path/to/input.txt
+../../../julia --threads 32 job.jl
